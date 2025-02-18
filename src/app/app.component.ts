ac086232100b55fa7bc220f145cbea4f8a8dcc0b
Laurent Tshisekedi <Laurent.N.Tshisekedi@gmail.com>
2025-02-18 16:17:52 -0600

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  DestroyRef,
+  OnInit,
+  effect,
+  inject,
+  signal,
+} from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+
+import { Observable, interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  clickCount = signal(0);
+  clickCount$ = toObservable(this.clickCount);
+  interval$ = interval(1000);
+  intervalSignal = toSignal(this.interval$);
 
+  customInterval$ = new Observable((subscriber) => {
+    setInterval(() => {
+      console.log('Emitting new value...');
+      subscriber.next({ message: 'New value' });
+    }, 5000);
+  });
+
+  private destroyRef = inject(DestroyRef);
+
+  constructor() {
+    // effect(() => {
+    //   console.log(`Clicked button ${this.clickCount()} times.`);
+    // });
+  }
+
+  ngOnInit(): void {
+    // const subscription = interval(500000)
+    //   .pipe(map((val) => val * 2))
+    //   .subscribe({
+    //     next: (val) => console.log(val),
+    //     // error: (err) => console.error(err),
+    //     // complete: () => console.log('completed'),
+    //   });
+    // this.destroyRef.onDestroy(() => {
+    //   subscription.unsubscribe();
+    // });
+    const subscription = this.clickCount$.subscribe({
+      next: (val) => console.log(`Clicked button ${this.clickCount()} times.`),
+    });
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+
+  onClick() {
+    this.clickCount.update((prevCount) => prevCount + 1);
+  }
 }
+
+//The DestroyRef is a built-in service which provides a way to run cleanup logic when the component is destroyed.
